6c5e6f404ad0c33cea9b81c20a41fc2170346751
FIRST1 <firstpope5604@gmail.com>
2020-01-06 11:50:53 +0700

    ปริ้นท์เพิ่ม

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
+        // Prints "Hello, World eiei" to the terminal window.
         System.out.println("Hello, World");
+	System.out.println("FirstfirstfurstFurst");
     }
 
